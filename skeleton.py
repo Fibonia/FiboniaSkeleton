68a4b5f912794f8f871f089e713e712b842fd3f1
Abel Yagubyan <43325291+Abelo9996@users.noreply.github.com>
2021-01-11 00:42:06 -0800

    Create skeleton.py

---
+++ skeleton.py
@@ -0,0 +1,27 @@
+def referral():
+    """ Code used to generate a referral code for every single account on Fibonia """
+    #Your Code Here
+    
+def otp():
+    """ Code used to generate a one-time password for every single appointment on Fibonia """
+    #Your Code Here
+    
+def discount():
+    """ Code used to generate a discount code to be used in Fibonia """
+    #Your Code Here
+    
+def select():
+    """ Extracts data from a database """
+    #Your Code Here
+
+def update():
+    """ Updates data in a database """
+    #Your Code Here
+
+def delete():
+    """ Deletes data from a database """
+    #Your Code Here
+    
+def insert():
+    """ Inserts new data into a database """
+    #Your Code Here
